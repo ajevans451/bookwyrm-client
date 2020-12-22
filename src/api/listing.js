fd103d702a58f56f8b4d65daf542eac9bb1a9744
ajevans <ajecoder@gmail.com>
2020-12-22 15:05:27 -0500

    add listing axios calls to own file

--- src/api/listing.js
+++ src/api/listing.js
@@ -1,2 +1,20 @@
 import axios from 'axios'
 import apiUrl from '../apiConfig'
+
+export const listingIndex = (listing, user) => {
+  return axios({
+    method: 'GET',
+    url: apiUrl + '/listings',
+    headers: {
+      'Authorization': `Token token=${user.token}`
+    },
+    data: {
+      listing: {
+        title: listing.title,
+        description: listing.description,
+        sellPrice: listing.sellPrice,
+        minStartingBid: listing.minStartingBid
+      }
+    }
+  })
+}
